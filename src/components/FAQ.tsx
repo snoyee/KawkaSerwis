58d46c5c7bfd299506ba48f65df6175f04378293
snoyee <gwosdekdawid@gmail.com>
2024-05-07 22:34:14 +0200

    save

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -12,20 +12,71 @@ const OurOffer = () => {
         <h1>FAQ 🔎</h1>
         <div className="QuestionDiv">
           <div className="Question">
-            <h2>1. Dlaczego jesteśmy zajebiści?</h2>
-            <p>Bo pracuje tu Sławomir Cieślicki</p>
+            <h2>1. Pytanie?</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. In
+              cursus turpis massa tincidunt dui ut ornare lectus sit. Erat
+              pellentesque adipiscing commodo elit at imperdiet dui accumsan.
+              Diam vel quam elementum pulvinar etiam. Mattis ullamcorper velit
+              sed ullamcorper. Turpis egestas sed tempus urna et pharetra
+              pharetra. Ac turpis egestas sed tempus urna et. At tempor commodo
+              ullamcorper a lacus vestibulum sed. Vulputate enim nulla aliquet
+              porttitor lacus luctus. Pretium vulputate sapien nec sagittis
+              aliquam malesuada bibendum. Velit sed ullamcorper morbi tincidunt.
+              Odio
+            </p>
           </div>
           <div className="Question">
-            <h2>2. Czy naprawimy twój sprzęt?</h2>
-            <p>Człowiekuu my nie wiemy co to elektronarzędzie nawet</p>
+            <h2>2. Pytanie?</h2>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. In
+              cursus turpis massa tincidunt dui ut ornare lectus sit. Erat
+              pellentesque adipiscing commodo elit at imperdiet dui accumsan.
+              Diam vel quam elementum pulvinar etiam. Mattis ullamcorper velit
+              sed ullamcorper. Turpis egestas sed tempus urna et pharetra
+              pharetra. Ac turpis egestas sed tempus urna et. At tempor commodo
+              ullamcorper a lacus vestibulum sed. Vulputate enim nulla aliquet
+              porttitor lacus luctus. Pretium vulputate sapien nec sagittis
+              aliquam malesuada bibendum. Velit sed ullamcorper morbi tincidunt.
+              Odio
+            </p>
           </div>
           <div className="Question">
-            <h2>3. Jakie koszty?</h2>
-            <p>Zadzwoń dogadamy się ;v</p>
+            <h2>3. Pytanie?</h2>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. In
+              cursus turpis massa tincidunt dui ut ornare lectus sit. Erat
+              pellentesque adipiscing commodo elit at imperdiet dui accumsan.
+              Diam vel quam elementum pulvinar etiam. Mattis ullamcorper velit
+              sed ullamcorper. Turpis egestas sed tempus urna et pharetra
+              pharetra. Ac turpis egestas sed tempus urna et. At tempor commodo
+              ullamcorper a lacus vestibulum sed. Vulputate enim nulla aliquet
+              porttitor lacus luctus. Pretium vulputate sapien nec sagittis
+              aliquam malesuada bibendum. Velit sed ullamcorper morbi tincidunt.
+              Odio
+            </p>
           </div>
           <div className="Question">
-            <h2>4. Ile średnio trwa naprawa?</h2>
-            <p>Jak nam się zachce naprawić</p>
+            <h2>4. Pytanie?</h2>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. In
+              cursus turpis massa tincidunt dui ut ornare lectus sit. Erat
+              pellentesque adipiscing commodo elit at imperdiet dui accumsan.
+              Diam vel quam elementum pulvinar etiam. Mattis ullamcorper velit
+              sed ullamcorper. Turpis egestas sed tempus urna et pharetra
+              pharetra. Ac turpis egestas sed tempus urna et. At tempor commodo
+              ullamcorper a lacus vestibulum sed. Vulputate enim nulla aliquet
+              porttitor lacus luctus. Pretium vulputate sapien nec sagittis
+              aliquam malesuada bibendum. Velit sed ullamcorper morbi tincidunt.
+              Odio
+            </p>
           </div>
         </div>
         <div style={{ height: "200px" }}></div>
