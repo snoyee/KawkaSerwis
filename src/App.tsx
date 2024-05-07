c521515c68c9d37b57fb8510e5d5d56ba08d62ea
snoyee <gwosdekdawid@gmail.com>
2024-05-07 20:52:57 +0200

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom";
 import Home from "./components/OurOffer";
 import Shop from "./components/Shop";
 import Repair from "./components/Repair";
@@ -9,18 +9,18 @@ import Error404 from "./components/Error404";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
-          <Route path="/KawkaSerwis/" element={<Home />}></Route>
-          <Route path="/KawkaSerwis/Oferta" element={<Home />}></Route>
-          <Route path="/KawkaSerwis/Oferta/Sklep" element={<Shop />}></Route>
-          <Route path="/KawkaSerwis/Oferta/Serwis" element={<Repair />}></Route>
-          <Route path="/KawkaSerwis/Lokalizacja" element={<Location />}></Route>
-          <Route path="/KawkaSerwis/FAQ" element={<FAQ />}></Route>
-          <Route path="/KawkaSerwis/OFirmie" element={<About />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/Oferta" element={<Home />}></Route>
+          <Route path="/Oferta/Sklep" element={<Shop />}></Route>
+          <Route path="/Oferta/Serwis" element={<Repair />}></Route>
+          <Route path="/Lokalizacja" element={<Location />}></Route>
+          <Route path="/FAQ" element={<FAQ />}></Route>
+          <Route path="/OFirmie" element={<About />}></Route>
           <Route path="*" element={<Error404 />}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
